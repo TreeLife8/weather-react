a51ce938b81d695fc3765a6a53c7d5223444e0f4
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-09-05 21:23:47 +1000

    Updated the whole strcture and forcast structure and added axios live data and activated the search button

--- src/App.js
+++ src/App.js
@@ -1,189 +1,290 @@
 import "./App.css";
 import React, { useState } from "react";
 import axios from "axios";
-import WeeklyForcast from "./WeeklyForcast";
+// import WeeklyForcast from "./WeeklyForcast";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 export default function App() {
-  const [city, setCity] = useState("");
-  const [temperature, setTemperature] = useState("");
-  // const [timestamp, setTimestamp] = useState("");
-  const [conditions, setConditions] = useState("");
+  const [timestamp, setTimestamp] = useState("");
+  const [weather, setWeather] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState("sydney");
+  // const [forcast, setForcast] = useState("");
 
-  // function formatDay() {
-  //   let time = new Date();
-  //   let days = [
-  //     "Monday",
-  //     "Tuesday",
-  //     "Wednesday",
-  //     "Thursday",
-  //     "Friday",
-  //     "Saturday",
-  //     "Sunday",
-  //   ];
-  //   setTimestamp({
-  //     minutes: time.getMinutes,
-  //     //  < 10 ? time.getMinutes : `0${time.getMinutes}`,
-  //     hours: time.getHours,
-  //     // < 10 ? time.getHours : `0${time.getHours}`,
-  //     day: days[time.getDay],
-  //   });
-  //   // setDay(days[time.getDay]);
-  //   console.log(time);
-  // }
-  displayForcast(`sydney`);
+  function formatDay(timeStamp) {
+    let time = new Date(timeStamp);
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    setTimestamp({
+      minutes:
+        time.getMinutes() > 10 ? time.getMinutes() : `0${time.getMinutes()}`,
+      hours: time.getHours() > 10 ? time.getHours() : `0${time.getHours()}`,
+      day: days[time.getDay()],
+      date: time.getDate(),
+      month: months[time.getMonth()],
+      year: time.getFullYear(),
+    });
+  }
 
   function handleResponse(response) {
-    setTemperature(Math.round(response.data.main.temp));
     setCity(response.data.name);
-    setConditions({
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
       wind: response.data.wind.speed,
       feelsLike: Math.round(response.data.main.feels_like),
       humidity: response.data.main.humidity,
-      description: response.data.main.description,
+      description: response.data.weather.description,
       maxTemp: Math.round(response.data.main.temp_max),
       minTemp: Math.round(response.data.main.temp_min),
     });
-    console.log(response.data);
-    // formatDay(response.data.main);
+    setLoading(true);
+    // console.log(response.data);
+    formatDay(response.data.dt * 1000);
   }
-
-  function displayForcast(city) {
-    let apiKey = `2f5ed0987c11d8af0a71b4472673fde7`;
+  // function handleForcastResponse(response) {
+  //   setForcast({
+  //     maxTemp: Math.round(response.data.list[0])
+  //   });
+  //   console.log(response.data);
+  // }
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchWeather(city);
+  }
+  function searchWeather(city) {
+    let apiKey = `3fb188379e6ffcf616e7cdbd010c6434`;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-7">
-            <div className="Search">
-              <form>
-                <div className="row">
-                  <div className="col-7 search">
-                    <input
-                      className="form-control"
-                      type="search"
-                      placeholder="Enter a city..."
-                      autoFocus
-                    ></input>
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+  if (loading) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-7">
+              <div className="Search">
+                <form onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-7 search">
+                      <input
+                        className="form-control"
+                        type="search"
+                        placeholder="Enter a city..."
+                        autoFocus
+                        onChange={changeCity}
+                      ></input>
+                    </div>
+                    <div className="col-5 search">
+                      <button
+                        className="btn searchButton  btn-control"
+                        value="submit"
+                      >
+                        Search
+                      </button>
+                    </div>
                   </div>
-                  <div className="col-5 search">
-                    <button
-                      className="btn searchButton  btn-control"
-                      value="submit"
-                    >
-                      Search
-                    </button>
-                  </div>
-                </div>
-              </form>
+                </form>
+              </div>
+            </div>
+            <div className="col-sm-2 search">
+              <div className="CurrentLocation ">
+                <button className="btn btn-control">Current</button>
+              </div>
+            </div>
+            <div className="col-sm-3 search icon">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="current weather icon"
+              />
             </div>
           </div>
-          <div className="col-sm-2 search">
-            <div className="CurrentLocation ">
-              <button className="btn btn-control">Current</button>
+          <div className="row">
+            <div className="col-6 TimeDate">
+              Last updated:
+              <div className="day">
+                {timestamp.day}
+                <span className="time">
+                  {" "}
+                  {timestamp.hours}:{timestamp.minutes}
+                </span>
+              </div>
+              <div className="date">
+                {timestamp.date} {timestamp.month} {timestamp.year}
+              </div>
+            </div>
+            <div className="col-2"></div>
+            <div className="col-4">
+              <div className="MaxMinTemps">
+                <div className="maxTemp">Max Temp: {weather.maxTemp}°C</div>
+                <div className="minTemp">Min Temp: {weather.minTemp}°C</div>
+              </div>
             </div>
           </div>
-          <div className="col-sm-3 search icon">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="current weather icon"
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-6 TimeDate">
-            Last updated:
-            <div className="day">
-              {/* {timestamp.day} */}
-              <span className="time">
-                {/* {timestamp.hours}:{timestamp.minutes} */}
+          <div className="Forcast">
+            <div className="cityName">{city}</div>
+            <div className="forcastDescription">{weather.description}</div>
+            <div className="temperature">
+              {weather.temperature}°
+              <span className="unit">
+                <a className=" celcius" href="/">
+                  C
+                </a>
+                {""}|{""}
+                <a className="active fehren" href="/">
+                  F
+                </a>
               </span>
             </div>
-            <div className="date">16 August 2021</div>
+            <div
+              className="btn-group"
+              role="group"
+              aria-label="Basic radio toggle button group"
+            >
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio1"
+              />
+              <label className="btn btn-outline-secondary" for="btnradio1">
+                HUMIDITY
+                <div className="humidity" id="humidity">
+                  {weather.humidity}%
+                </div>
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio2"
+              />
+              <label className="btn btn-outline-secondary" for="btnradio2">
+                FEELS LIKE
+                <div className="feelsLike" id="feels-like">
+                  {weather.feelsLike}°
+                </div>
+              </label>
+              <input
+                type="radio"
+                className="btn-check"
+                name="btnradio"
+                id="btnradio3"
+              />
+              <label className="btn btn-outline-secondary" for="btnradio3">
+                WINDSPEED
+                <div className="windspeed" id="windspeed">
+                  {weather.wind} km/h
+                </div>
+              </label>
+            </div>
           </div>
-          <div className="col-2"></div>
-          <div className="col-4">
-            <div className="MaxMinTemps">
-              <div className="maxTemp">Max Temp: {conditions.maxTemp}°C</div>
-              <div className="minTemp">Min Temp: {conditions.minTemp}°C</div>
+          <hr />
+          <div className="row">
+            {/* <WeeklyForcast city={city} loaded={true} /> */}
+            <div className="col-2  ps-4">
+              <span className="maxTemp forcast">25°</span>
+              <span className="minTemp forcast">20°</span>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="current weather icon"
+              />
+              <div className="forcastDay">Tue</div>
+            </div>
+            <div className="col-2  ps-4">
+              <span className="maxTemp forcast">25°</span>
+              <span className="minTemp forcast">20°</span>
+              <img
+                className="forcastIcon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="current weather icon"
+              />
+              <div className="forcastDay">Wed</div>
+            </div>
+            <div className="col-2  ps-4">
+              <span className="maxTemp forcast">25°</span>
+              <span className="minTemp forcast">20°</span>
+              <img
+                className="forcastIcon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="current weather icon"
+              />
+              <div className="forcastDay">Thu</div>
+            </div>
+            <div className="col-2  ps-4">
+              <span className="maxTemp forcast">25°</span>
+              <span className="minTemp forcast">20°</span>
+              <img
+                className="forcastIcon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="current weather icon"
+              />
+              <div className="forcastDay">Fri</div>
+            </div>
+            <div className="col-2  ps-4">
+              <span className="maxTemp forcast">25°</span>
+              <span className="minTemp forcast">20°</span>
+              <img
+                className="forcastIcon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="current weather icon"
+              />
+              <div className="forcastDay">Sat</div>
             </div>
           </div>
         </div>
-        <div className="Forcast">
-          <div className="cityName">{city}</div>
-          <div className="forcastDescription">{conditions.description}</div>
-          <div className="temperature">
-            {temperature}°
-            <span className="unit">
-              <a className=" celcius" href="/">
-                C
-              </a>
-              {""}|{""}
-              <a className="active fehren" href="/">
-                F
-              </a>
-            </span>
-          </div>
-          <div
-            className="btn-group"
-            role="group"
-            aria-label="Basic radio toggle button group"
-          >
-            <input
-              type="radio"
-              className="btn-check"
-              name="btnradio"
-              id="btnradio1"
-            />
-            <label className="btn btn-outline-secondary" for="btnradio1">
-              HUMIDITY
-              <div className="humidity" id="humidity">
-                {conditions.humidity}%
-              </div>
-            </label>
-            <input
-              type="radio"
-              className="btn-check"
-              name="btnradio"
-              id="btnradio2"
-            />
-            <label className="btn btn-outline-secondary" for="btnradio2">
-              FEELS LIKE
-              <div className="feelsLike" id="feels-like">
-                {conditions.feelsLike}°
-              </div>
-            </label>
-            <input
-              type="radio"
-              className="btn-check"
-              name="btnradio"
-              id="btnradio3"
-            />
-            <label className="btn btn-outline-secondary" for="btnradio3">
-              WINDSPEED
-              <div className="windspeed" id="windspeed">
-                {conditions.wind} km/h
-              </div>
-            </label>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-1"></div>
-          <div className="col-2">
-            <WeeklyForcast />
-          </div>
-          <div className="col-1"></div>
+        <div className="gitHubLink">
+          <a href="https://github.com/TreeLife8/Banana-Weather-App-">
+            Open-source code
+          </a>
+          , by Regina Maher
         </div>
       </div>
-      <div className="gitHubLink">
-        <a href="https://github.com/TreeLife8/Banana-Weather-App-">
-          Open-source code
-        </a>
-        , by Regina Maher
+    );
+  } else {
+    let apiKey = `3fb188379e6ffcf616e7cdbd010c6434`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    // let apiForcastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    // axios.get(apiForcastUrl).then(handleForcastResponse);
+    return (
+      <div className="container row">
+        <div className="col-5"></div>
+        <div className="col-4">
+          <Loader
+            type="Puff"
+            color="#757a79"
+            height={200}
+            width={200}
+            timeout={4000}
+          />
+        </div>
+        <div className="col-3"></div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/WeeklyForcast.js
+++ src/WeeklyForcast.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function WeeklyForcast(propms) {
-  return <div className="WeeklyForcast">hi</div>;
+export default function WeeklyForcast(promps) {
+  return (
+    <div className="WeeklyForcast row">
+      {props.forcast.map((forcast, index) => {
+        <div className="col-2" key={index}></div>;
+      })}
+    </div>
+  );
 }
